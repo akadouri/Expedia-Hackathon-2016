7b320459cdf18b38ad1db27ce8fdcd6c84843b34
Ckrisirkc <gotaques@yahoo.com>
2016-01-30 13:41:36 -0800

    added models

---
+++ app/src/main/java/com/arielandchris/expediahackathon/model/GeoSearch.java
@@ -0,0 +1,138 @@
+
+package com.arielandchris;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("org.jsonschema2pojo")
+public class GeoSearch {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("source")
+    @Expose
+    private Source source;
+    @SerializedName("position")
+    @Expose
+    private Position position;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    /**
+     * 
+     * @return
+     *     The id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id
+     *     The id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * @return
+     *     The type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * 
+     * @param type
+     *     The type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return
+     *     The source
+     */
+    public Source getSource() {
+        return source;
+    }
+
+    /**
+     * 
+     * @param source
+     *     The source
+     */
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    /**
+     * 
+     * @return
+     *     The position
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * 
+     * @param position
+     *     The position
+     */
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    /**
+     * 
+     * @return
+     *     The status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * 
+     * @param status
+     *     The status
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
