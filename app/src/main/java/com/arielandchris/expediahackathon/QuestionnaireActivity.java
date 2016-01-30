b24c516b6a886a98c6955cc1dbb9d5012403cd9b
Ariel Kadouri <ariel@arielsartistry.com>
2016-01-29 21:27:14 -0800

    added questionaire activity

---
+++ app/src/main/java/com/arielandchris/expediahackathon/QuestionnaireActivity.java
@@ -0,0 +1,9 @@
+package com.arielandchris.expediahackathon;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ariel on 1/29/16.
+ */
+public class QuestionnaireActivity extends AppCompatActivity {
+}
