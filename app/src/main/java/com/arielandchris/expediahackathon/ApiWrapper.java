3f99918216e1722f6c51ea9ea4a757213f2efabb
Ckrisirkc <gotaques@yahoo.com>
2016-01-30 13:41:02 -0800

    models

--- app/src/main/java/com/arielandchris/expediahackathon/ApiWrapper.java
+++ app/src/main/java/com/arielandchris/expediahackathon/ApiWrapper.java
@@ -11,6 +11,7 @@ import retrofit2.Retrofit;
 // http://terminal2.expedia.com:80/x/mflights/search?departureDate=2016-02-2&departureAirport=SEA&arrivalAirport=JFK
 // Things to do
 // http://terminal2.expedia.com:80/x/activities/search?location=London&startDate=2015-08-08&endDate=2015-08-18
+// http://terminal2.expedia.com:80/x/geo/features?within=50km&lat=37.777363&lng=-122.453269&type=airport
 public class ApiWrapper {
     private static final string API_KEY;
     private Gson gson;
@@ -22,7 +23,13 @@ public class ApiWrapper {
         gson = new Gson();
     }
 
-    public thingsToDo(String searchTerm) {
-        thingsToDo(searchTerm, "none", "none")
+    public GeoSearch geoSearch(String )
+
+    public String thingsToDo(String searchTerm) {
+        thingsToDo(searchTerm, "none", "none");
+    }
+    public String thingsToDo(String searchTerm, String startDate, String endDate) {
+
+        return "temp";
     }
 }
