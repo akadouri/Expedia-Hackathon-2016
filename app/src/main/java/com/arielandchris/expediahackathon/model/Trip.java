0de70b4f17a5cc6fc8571cddf09301048b40eefe
Ckrisirkc <gotaques@yahoo.com>
2016-01-31 11:02:50 -0800

    Trip model ready for use

--- app/src/main/java/com/arielandchris/expediahackathon/model/Trip.java
+++ app/src/main/java/com/arielandchris/expediahackathon/model/Trip.java
@@ -1,15 +1,48 @@
 package com.arielandchris.expediahackathon.model;
 
+import com.arielandchris.expediahackathon.model.t2d.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Chris on 1/31/2016.
  */
 public class Trip {
-    private Airport airport;
+    private Airport origin, dest;
+    private List<Package> packageList;
+    private List<Activity> activityList;
     public Trip() {
-        airport = null;
+        origin = null;
+        dest = null;
+        packageList = new ArrayList<>();
+        activityList = new ArrayList<>();
     }
 
-    public void setAirport(Airport airport) {
-        this.airport = airport;
+    public Trip(Trip other) {
+        this.origin = other.origin;
+        this.dest = other.dest;
     }
+
+    public void setOriginAP(Airport airport) {
+        this.origin = airport;
+    }
+    public void setDestAP(Airport airport) {
+        this.dest = airport;
+    }
+    public Airport getOriginAP() { return this.origin; }
+    public Airport getDestAP() { return this.dest; }
+
+    public void addPackage(Package pkg) {
+        packageList.add(pkg);
+    }
+    public List<Package> getPackageList() { return this.packageList; }
+
+    public void addActivity(Activity activity) {
+        activityList.add(activity);
+    }
+    public List<Activity> getActivityList() { return this.activityList; }
+    
+
+
 }
